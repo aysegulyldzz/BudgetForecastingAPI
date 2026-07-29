df2ef71a64ea0f876c69c9767010d04d83bc2e75
ayhantz <ayhan@yerel.com>
2026-07-30 01:52:11 +0300

    Katman 3: BudgetController.cs eklendi

---
+++ BudgetForecastingAPI/Controllers/BudgetController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BudgetApp.DTOs;
+using BudgetApp.Services; 
+
+namespace BudgetForecastingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetController : ControllerBase
+    {
+    }
+}
